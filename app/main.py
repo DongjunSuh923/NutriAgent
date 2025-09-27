26ee0de4d24b2cafd2ff492b6aeb0ebf2ec80a89
서동준 <1dongjun26@gmail.com>
2025-09-28 03:19:20 +0900

    feat: Added AutoComplete function while searching, Now the user can view Details about the food by cliking the text

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
-import psycopg2
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routers import foods
 
 app = FastAPI()
+
+app.include_router(foods.router)
 
 app.add_middleware(
     CORSMiddleware,
@@ -11,40 +13,3 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-def get_connection():
-    return psycopg2.connect(
-        host="localhost",
-        dbname="nutri_db",
-        user="postgres",
-        password="1234",
-        port=5432
-    )
-
-@app.get("/search")
-async def search(query: str):
-    conn = get_connection()
-    cur = conn.cursor()
-
-    cur.execute("""
-        SELECT id, name, calories, protein, carbs, fat 
-        FROM foods
-        WHERE name LIKE %s
-    """, ('%' + query + '%',))
-
-    rows = cur.fetchall()
-    cur.close()
-    conn.close()
-
-    results = []
-    for row in rows:
-        results.append({
-            "id": row[0],
-            "name": row[1],
-            "calories": row[2],
-            "protein": row[3],
-            "carbs": row[4],
-            "fat": row[5],
-        })
-
-    return results
